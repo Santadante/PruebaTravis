641783885d1e66215b5276376ddf63d004a7d6ac
Santadante <49271860+Santadante@users.noreply.github.com>
2020-04-22 08:49:59 +0200

    test resta

---
+++ test.js
@@ -0,0 +1,20 @@
+// Ejemplo de test utilizando el módulo assert disponible en NodeJS
+
+// Cargar el módulo assert
+var assert = require('assert');
+
+// Cargar el módulo con las funciones para testear
+var func = require('../funciones.js');
+
+// Test
+it('comprobar función suma', function() {
+	assert.equal(func.suma(1,3), 4);
+	assert.equal(func.suma(-1,3), 2);
+	assert.equal(func.suma(8,-3), 5);
+});
+
+it('comprobar función resta', function() {
+	assert.equal(func.suma(3,1), 2);
+	assert.equal(func.suma(8,3), 5);
+	assert.equal(func.suma(-5,-3), 8);
+});
