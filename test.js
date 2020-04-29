a1c13319962baccecbe75e9e051bdb7dc8f9ab83
Santadante <49271860+Santadante@users.noreply.github.com>
2020-04-29 08:38:03 +0200

    un cambio

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // Ejemplo de test utilizando el módulo assert disponible en NodeJS
 
 // Cargar el módulo assert
-var assert = require('assert');
+var assert = require('assert' );
 
 // Cargar el módulo con las funciones para testear
 var func = require('../funciones.js');
